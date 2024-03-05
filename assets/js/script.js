ce9f4e719c1ccb404d6a39e8aae1f0a1d153d750
Pablo Jelvez <pjelvezj@gmail.com>
2024-03-05 12:01:49 -0300

    Se implementan funcionalidades de JQuery (tooltip, alert, click y dclick) y se corrigen detalles generales de la pagina

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+// Script para funcionalidad del tooltip de boostrap
+const tooltipTriggerList = document.querySelectorAll(
+  '[data-bs-toggle="tooltip"]'
+);
+const tooltipList = [...tooltipTriggerList].map(
+  (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+);
+
+// Ready function de JQuery
+$(function () {
+//   $(document).scroll(function () {
+//     var a = $(".navbar").offset().top;
+//     if ($(this).scrollTop() > a) {
+//       $(".navbar").css({ "background-color": "#2796a3" });
+//     } else {
+//       $(".navbar").css({ "background-color": "transparent" });
+//     }
+//   });
+
+  // Funcionalidad alert para el boton de la sección contacto
+  $("#enviarCorreo").on("click", function () {
+    alert("El correo fue enviado correctamente.");
+  });
+
+  // Funcionalidad que transforma a color rojo todos los textos de las cards al hacer doble click en los titulos de las secciones
+  $("h3").on("dblclick", function () {
+    $(".card-text").css("color", "red");
+  });
+
+  // Funcionalidad que oculta las cards de la sección quienes somos al hacer click en el titulo
+  $("#titleQuienesSomos").on("click", function () {
+    $(".cards").toggle("slow");
+  });
+});
